89a6e80f4d08be77f662b05fb1879fe6667353e5
fitz95 <fitzgeraldnsong@gmail.com>
2025-11-20 09:59:28 +0100

    added auto migrate to build and player migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20241125_222020_initial from './20241125_222020_initial';
 import * as migration_20241214_124128 from './20241214_124128';
+import * as migration_20251120_085209 from './20251120_085209';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20241214_124128.up,
     down: migration_20241214_124128.down,
-    name: '20241214_124128'
+    name: '20241214_124128',
+  },
+  {
+    up: migration_20251120_085209.up,
+    down: migration_20251120_085209.down,
+    name: '20251120_085209'
   },
 ];
