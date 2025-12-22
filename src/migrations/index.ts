f065f26dc88f92e4a2bbe42b18bdbf4e265fe475
fitz95 <fitzgeraldnsong@gmail.com>
2025-12-22 07:40:17 +0100

    fix migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20251222_063114 from './20251222_063114';
+import * as migration_20251222_063955 from './20251222_063955';
 
 export const migrations = [
   {
-    up: migration_20251222_063114.up,
-    down: migration_20251222_063114.down,
-    name: '20251222_063114'
+    up: migration_20251222_063955.up,
+    down: migration_20251222_063955.down,
+    name: '20251222_063955'
   },
 ];
