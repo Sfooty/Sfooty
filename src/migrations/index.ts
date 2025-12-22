4753bafea6ef7ec017d85e9f8a959a866d75e147
fitz95 <fitzgeraldnsong@gmail.com>
2025-12-22 07:47:08 +0100

    fix migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20251222_063955 from './20251222_063955';
+import * as migration_20251222_064652 from './20251222_064652';
 
 export const migrations = [
   {
-    up: migration_20251222_063955.up,
-    down: migration_20251222_063955.down,
-    name: '20251222_063955'
+    up: migration_20251222_064652.up,
+    down: migration_20251222_064652.down,
+    name: '20251222_064652'
   },
 ];
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -62,6 +62,7 @@ export default buildConfig({
     pool: {
       connectionString: process.env.DATABASE_URI || '',
     },
+    push: false,
   }),
   collections: [Pages, Posts, Media, Categories, Users, Comments, Players],
   cors: [getServerSideURL()].filter(Boolean),
